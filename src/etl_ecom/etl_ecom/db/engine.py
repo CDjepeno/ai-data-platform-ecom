29322074143817d78810033c5228ea971765c951
CDjepeno <djepenodev@gmail.com>
2026-06-04 02:40:53 +0200

    :hammer: add certification for ssl

--- src/etl_ecom/etl_ecom/db/engine.py
+++ src/etl_ecom/etl_ecom/db/engine.py
@@ -61,7 +61,8 @@ def get_duckdb_connection() -> duckdb.DuckDBPyConnection:
                     password={settings.postgres_password}
                     host={settings.postgres_host}
                     port={settings.postgres_port}
-                    sslmode=require
+                    sslmode=verify-ca
+                    sslrootcert=/app/certs/ovh-postgres.pem
                 '
                 AS postgres_db
                 (TYPE postgres);
